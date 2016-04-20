094fac0e84d4720f7be713a7999c71443ca529cb
Guillermo Menéndez <guillermo.menendez@touchvie.com>
2016-04-20 15:31:28 +0200

    Added scripts to perform full DB snapshot and backup

---
+++ bin/full-db-snapshot.js
@@ -0,0 +1,104 @@
+#!/usr/bin/env node
+
+var args = require('minimist')(process.argv.slice(2));
+var s3urls = require('s3urls');
+var snapshot = require('../s3-snapshot');
+var queue = require('queue-async');
+var AWS = require('aws-sdk');
+var dateformat = require('dateformat');
+
+var S3_SEP = '/';
+
+function usage() {
+    console.error('');
+    console.error('Usage: full-db-snapshot <s3srcbucket> <s3dstbucket> [snapshottag]');
+    console.error(' - s3srcbucket: s3 source bucket where tables are backed up');
+    console.error(' - s3dstbucket: s3 destination bucket where snapshot will be stored');
+    console.error(' - snapshottag: optional tag for the snapshot. "yyyymmdd" by default');
+}
+
+if (args.help) {
+    usage();
+    process.exit(0);
+}
+
+var s3src = args._[0];
+if (!s3src) {
+    console.error('Must provide an s3srcbucket');
+    usage();
+    process.exit(1);
+}
+s3src = s3urls.fromUrl(s3src);
+
+var s3dst = args._[1];
+if (!s3dst) {
+    console.error('Must provide an s3dstbucket');
+    usage();
+    process.exit(1);
+}
+s3dst = s3urls.fromUrl(s3dst);
+
+var snapshottag = args._[2];
+if (!snapshottag) {
+    snapshottag = dateformat(new Date(), "yyyymmdd");
+    console.error('Using default snapshot tag: ' + snapshottag);
+}
+
+var queue = queue();
+
+var snapshotTable = (table) => {
+    
+    queue.defer((next) => {
+        var tag = [snapshottag, table].join(S3_SEP).slice(0, -1);
+        console.log("Creating snapshot " + tag);
+        snapshot({
+            //log: '',
+            //logger: '',
+            source: {
+                bucket: s3src.Bucket,
+                prefix: table
+            },
+            destination: {
+                bucket: s3dst.Bucket,
+                key: tag
+            },
+            maxRetries: 1000
+        }, (err) => {
+            if (err)
+                console.error(err);
+            console.log("Finished " + table + " snapshot");
+            next();
+        });
+    });    
+}
+
+var s3 = new AWS.S3();
+
+var listTables = (lastKey) => {
+    
+    s3.listObjects({
+        Bucket: s3src.Bucket,
+        Delimiter: S3_SEP,
+        EncodingType: 'url',
+        Marker: lastKey,
+        MaxKeys: 100,
+        Prefix: 'preproduction_actor'
+    }, (err, data) => {
+        if (err) {
+            console.log(err, err.stack);
+        } else {
+            data.CommonPrefixes.map((prefix) => { 
+                snapshotTable(prefix.Prefix)
+            })
+            if (data.IsTruncated) {
+                listTables(data.NextMarker);
+            } else {
+                queue.awaitAll((err, data) => { 
+                    if (err) console.log(err);
+                });
+            }
+        }
+    });
+}
+
+listTables(null);
